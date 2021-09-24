3eb6dbc86b653ebda76c369a7ff7d6a459d9cd4a
tivarus <53016303+tivarus@users.noreply.github.com>
2021-09-24 08:43:18 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,31 +5,37 @@
 
 int main() {
     char number[N] = {0}, copy[N] = {0};
-    scanf("%s", number);
+    scanf("%s", number); //вводим число
     int i = 0, k = 0;
-        if((number[i] != '.') & (number[i] != ',') & (strlen(number) < N)) {
-            while((number[i] != '.') & (number[i] != ',')) {
+        if((number[i] != '.') & (number[i] != ',') & (strlen(number) < N)) { 
+            //задаем оператор if, if выдает ошибку если число слишком большое или в его начале стоит .(,)
+            while((number[i] != '.') & (number[i] != ',')) { 
+                // последовательно перебираем все числа до точки и записываем их в массив copy
                 if(number[i] != '\0') {
                     copy[k] = number[i];
                     i = i+1;
                     k = i;
                     number[i] = number[i];
                 }
-                else {
+                else {   
+                    //если число задано без точки то выдает ошибку
                     break;
                 }
             }
-            if((number[i+1] != '\0'))  {
+            if((number[i+1] != '\0') &&((number[i+1] != '.') & (number[i+1] != ',')))  { 
+                //если в числе две и более точки то выдаст ошибку
                 copy[k] = '|';
                 i = i+1;
                 k = i;
-                while(number[i] != '\0') {
+                while(number[i] != '\0') { 
+                    //перебираем оставшееся число до \0 и записываем значения в copy
                     copy[k] = number[i];
                     i = i+1;
                     k = i;
                     number[i] = number[i];
                 }
-                puts(copy);
+                puts(copy); 
+                //выводим получившуюся строку
 
             }
             else {
