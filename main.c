20fc01c4ce34381a5376a9a5c2426ffad6df2754
tivarus <53016303+tivarus@users.noreply.github.com>
2021-09-29 23:32:18 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,70 +1,63 @@
 #include <stdio.h>
 #include <string.h>
 #define N 10
-#define err "incorrect number"
+#define ERROR "incorrect number"
 
 int main() {
     char number[N];
     gets(number);
     int i = 0, b;
-        if(number[i] != '.' && number[i] != ',' && strlen(number) < N) {
-        }
-        else {
-            b = 1;
-        }
-        while(number[i] != '.' && number[i] != ',' && b != 1) {
-            switch (number[i]) {
-                case '\0':
+    if(number[i] == '.' || number[i] == ',' || strlen(number) > N) {
+        b = 1;
+    }
+    while(number[i] != '.' && number[i] != ',' && b != 1) {
+        switch (number[i]) {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                i = i+1;
+                break;
+            default:
                 b = 1;
                 break;
-                case '0':
-                case '1':
-                case '2':
-                case '3':
-                case '4':
-                case '5':
-                case '6':
-                case '7':
-                case '8':
-                case '9':
-                    i = i+1;
-                    break;
-                default:
-                    b = 1;
-                    break;
-            }
         }
-        number[i] = '|';
-        i = i+1;
-        if (number[i] != '\0') {
-        } 
-        else {
-            b = 1;
+    }
+    number[i] = '|';
+    i = i+1;
+    if (number[i] == '\0') {
+        b = 1;
+    }
+    while(number[i] != '\0' && b != 1) {
+       switch (number[i]) {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                i = i+1;
+                break;
+            default:
+                b = 1;
+                break;
         }
-        while(number[i] != '\0' && b != 1) {
-           switch (number[i]) {
-                case '0':
-                case '1':
-                case '2':
-                case '3':
-                case '4':
-                case '5':
-                case '6':
-                case '7':
-                case '8':
-                case '9':
-                    i = i+1;
-                    break;
-                default:
-                    b = 1;
-                    break;
-                }
-            } 
-            if (b !=1) {
-                puts(number);
-            } 
-            else {
-                puts(err);
-            }
+    } 
+    if (b !=1) {
+        puts(number);
+    } 
+    else {
+        puts(ERROR);
+    }
     return 0;
 }
